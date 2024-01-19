9303d975037b641ea09254b9d240ac4972384db9
Adriano Porto <adriano.porto.a.j@gmail.com>
2024-01-19 15:18:12 -0300

    create user controller & authenticate controller

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { AuthenticateController } from './controllers/authenticate-controller'
+
+@Module({
+    imports: [ConfigModule.forRoot({
+        validate: environmentVariables => envSchema.parse(environmentVariables), // Checks if environment variables are valid 
+        isGlobal: true,
+    }),
+    AuthModule,
+    ],
+    controllers: [CreateAccountController, AuthenticateController],
+    providers: [PrismaService],
+})
+
+export class AppModule {}
